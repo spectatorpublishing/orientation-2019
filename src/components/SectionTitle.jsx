6b8b51bb27c5fb7ea5969edca22ce2e3a4f959e2
wangyyt1013 <yw3241@columbia.edu>
2019-08-20 15:18:26 -0400

    updated version of the component file

--- src/components/SectionTitle.jsx
+++ src/components/SectionTitle.jsx
@@ -1,21 +1,39 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 
+const MemberItem = styled.h4`
+  text-align: left;
+  color: white;
+`;
+const Title = styled.h2`
+  text-align : left;
+  padding-bottom : 0.6em;
+  color: white;
+`;
+const Container = styled.div`
+  text-align: left;
+  padding: 1em;
+  background: #106e75;
+  width: 9rem;
+`;
 const SectionTitle = (props) => {
   const { title, members } = props;
   return (
-    <div>
-      <div className="title">{title}</div>
+    <Container>
+      <div className="title">
+        <Title>{title}</Title>
+      </div>
       <div>
         {members.map((member) => (
-          <p>
+          <MemberItem>
             {member.split(',')[0]}
 ,
             <i>{member.split(',')[1]}</i>
-          </p>
+          </MemberItem>
         ))}
       </div>
-    </div>
+    </Container>
   );
 };
 
